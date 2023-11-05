39ed992e9d9dac72b3c6ce8e4c4f42ed342e900c
Palatin Bence <palatinb@gmail.com>
2023-11-05 23:13:42 +0100

    wip

--- src/main/java/eu/hansolo/fx/charts/areaheatmap/AreaHeatMap.java
+++ src/main/java/eu/hansolo/fx/charts/areaheatmap/AreaHeatMap.java
@@ -31,6 +31,7 @@ import javafx.beans.property.IntegerPropertyBase;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.ObjectPropertyBase;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
@@ -46,6 +47,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 
 @DefaultProperty("children")
@@ -69,6 +74,7 @@ public class AreaHeatMap extends Region {
     private static final double                  MINIMUM_HEIGHT   = 50;
     private static final double                  MAXIMUM_WIDTH    = 1024;
     private static final double                  MAXIMUM_HEIGHT   = 1024;
+    private static final int                          ROW_CHUNK = 75;
     private              double                  size;
     private              double                  width;
     private              double                  height;
@@ -402,26 +408,30 @@ public class AreaHeatMap extends Region {
         }
     }
 
-    private void draw(final int LIMIT, final double RESOLUTION) {
+    private Callable<Void> draw(final int startRow, final int LIMIT, final double RESOLUTION) {
         int limit        = LIMIT > points.size() ? points.size() : LIMIT + 1;
         double pixelSize = 2 * RESOLUTION;
 
-        ctx.clearRect(0, 0, width, height);
+        ctx.clearRect(0, startRow, width, startRow + ROW_CHUNK);
 
-        for (double y = 0 ; y < height ; y += RESOLUTION) {
+        for (double y = startRow ; y < startRow + ROW_CHUNK ; y += RESOLUTION) {
             for (double x = 0 ; x < width ; x += RESOLUTION) {
-                double value = getValueAt(limit, x, y);
-                if (value != -255) {
-                    Color          color    = getUseColorMapping() ? getColorForValue(value) : getColorForValue(value, isDiscreteColors());
-                    RadialGradient gradient = new RadialGradient(0, 0, x, y, RESOLUTION,
-                                                                 false, CycleMethod.NO_CYCLE,
-                                                                 new Stop(0, Color.color(color.getRed(), color.getGreen(), color.getBlue(), getHeatMapOpacity())),
-                                                                 new Stop(1, Color.color(color.getRed(), color.getGreen(), color.getBlue(), 0.0)));
-                    ctx.setFill(gradient);
-                    ctx.fillOval(x - RESOLUTION, y - RESOLUTION, pixelSize, pixelSize);
+                if (Helper.isInPolygon(x, y, polygon)) {
+
+                    double value = getValueAt(limit, x, y);
+                    if (value != -255) {
+                        Color          color    = getUseColorMapping() ? getColorForValue(value) : getColorForValue(value, isDiscreteColors());
+                        RadialGradient gradient = new RadialGradient(0, 0, x, y, RESOLUTION,
+                                false, CycleMethod.NO_CYCLE,
+                                new Stop(0, Color.color(color.getRed(), color.getGreen(), color.getBlue(), getHeatMapOpacity())),
+                                new Stop(1, Color.color(color.getRed(), color.getGreen(), color.getBlue(), 0.0)));
+                        ctx.setFill(gradient);
+                        ctx.fillOval(x - RESOLUTION, y - RESOLUTION, pixelSize, pixelSize);
+                    }
                 }
             }
         }
+        return null;
     }
 
     private void drawDataPoints() {
@@ -460,8 +470,25 @@ public class AreaHeatMap extends Region {
         }
     }
 
-    private void redraw() {
-        draw(getNoOfCloserInfluentPoints(), getQuality());
+    private void redraw()  {
+        List<Callable<Void>> tasks = new ArrayList<>();
+        double doubleNumber = (height / ROW_CHUNK);
+        int intPart = (int) doubleNumber;
+        int iterator = (doubleNumber - intPart > 0) ? intPart + 1 : intPart;
+
+        for (int j = 0; j < iterator; j++) {
+            tasks.add(draw(j * ROW_CHUNK, getNoOfCloserInfluentPoints(), getQuality()));
+        }
+
+        for (int i = 0; i < tasks.size(); i++) {
+            try {
+                if (tasks.get(i) != null) {
+                    tasks.get(i).call();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         if (getShowDataPoints()) { drawDataPoints(); }
     }
 }
